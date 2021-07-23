483832b99d7babd537cb05dcd2d462a8cf7b7b93
Nizar4790k <nizar4790k@gmail.com>
2021-07-23 10:33:18 -0400

    the script now extracts the images frame by frame and highligth (pre,col,post) frames

--- DOT_Iowa_Accident_Labels.py
+++ DOT_Iowa_Accident_Labels.py
@@ -25,7 +25,35 @@ def extract_frames(video_data):
 
     vidcap=cv2.VideoCapture(video_name) # Loading the video 
     
+    i=0
 
+    framerate = round (vidcap.get(5)) 
+    frame_start=int(times[0][1])*framerate
+    frame_end=(int(times[2][1])+1)*framerate
+    
+
+    for frame_number in range(frame_start,frame_end):
+        print(frame_number)
+        if(i>2):
+            break
+        
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,float((frame_number/framerate)*1000)) # Capturing the frame in the time specified in ms
+        success,image=vidcap.read()
+
+        if success:
+          
+
+            if (frame_number/framerate)==float(times[i][1]):
+                cv2.imwrite(video_name.split(".")[0]+"_"+times[i][0]+"_"+str(frame_number)+".jpg",image) # writing the image
+                i=i+1
+            else:
+                cv2.imwrite(video_name.split(".")[0]+"_"+str(frame_number)+".jpg",image)
+       
+
+            
+
+
+"""
     for time in times:
        
         vidcap.set(cv2.CAP_PROP_POS_MSEC,float(int(time[1])*1000)) # Capturing the frame in the time specified in ms
@@ -36,7 +64,7 @@ def extract_frames(video_data):
             
             frame_number = framerate * int(time[1])
             cv2.imwrite(video_name.split(".")[0]+"_"+time[0]+"_"+str(frame_number)+".jpg",image) # writing the image
-
+"""
 
     
 
@@ -49,7 +77,7 @@ os.chdir((os.environ['WORKING_DIRECTORY'])) # loading the working directory
 
 
 
-years = os.listdir()
+years = os.listdir() # listing years folders
 
 for year in years:
     os.chdir(year)
