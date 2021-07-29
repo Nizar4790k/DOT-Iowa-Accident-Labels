68e556a49ea6557139eecbae592684460f0758df
Nizar4790k <nizar4790k@gmail.com>
2021-07-29 17:08:54 -0400

    the script now works with multiple folders

--- DOT_Iowa_Accident_Labels.py
+++ DOT_Iowa_Accident_Labels.py
@@ -7,6 +7,21 @@ import cv2
 load_dotenv()
 
 
+
+def get_video_formats(data):
+    formats = list()
+    for video in data:
+        try:
+            format = video[4].split(".")[1] # This is the video format
+            formats.append(format)
+        except:
+            continue
+    
+    formats = set(formats)
+
+    return formats
+
+
 def read_csv(filename):
      with open(filename, 'r') as f:
     # reads csv into a list of lists
@@ -14,7 +29,8 @@ def read_csv(filename):
         return list(lines)[1:] #
 
 
-def extract_frames(video_data):
+
+def extract_frames(video_data,video_path,root):
     video_name = video_data[4]
     
     
@@ -23,17 +39,24 @@ def extract_frames(video_data):
     ["col",video_data[8]],
     ["post",video_data[9]]]
 
-    vidcap=cv2.VideoCapture(video_name) # Loading the video 
+
+    vidcap=cv2.VideoCapture(video_path) # Loading the video 
     
     i=0
 
     framerate = round (vidcap.get(5)) 
-    frame_start=int(times[0][1])*framerate
-    frame_end=(int(times[2][1])+1)*framerate
+    
+    try:
+
+        frame_start=int(times[0][1])*framerate
+        frame_end=(int(times[2][1])+1)*framerate
+    except:
+        print("The frame start or frame end are not int")
+        return
     
 
     for frame_number in range(frame_start,frame_end):
-        print(frame_number)
+       
         if(i>2):
             break
         
@@ -44,10 +67,10 @@ def extract_frames(video_data):
           
 
             if (frame_number/framerate)==float(times[i][1]):
-                cv2.imwrite(video_name.split(".")[0]+"_"+times[i][0]+"_"+str(frame_number)+".jpg",image) # writing the image
+                cv2.imwrite(root+"/"+video_name.split(".")[0]+"_"+times[i][0]+"_"+str(frame_number)+".jpg",image) # writing the image
                 i=i+1
             else:
-                cv2.imwrite(video_name.split(".")[0]+"_"+times[i][0]+"_"+str(frame_number)+".jpg",image)
+                cv2.imwrite(root+"/"+video_name.split(".")[0]+"_"+times[i][0]+"_"+str(frame_number)+".jpg",image)
        
 
             
@@ -73,25 +96,67 @@ def extract_frames(video_data):
 data = read_csv('DOT_Iowa_Accident_Labels_all.csv') # loading the csv
 
 
+
+formats = get_video_formats(data)
+
+
 os.chdir((os.environ['WORKING_DIRECTORY'])) # loading the working directory
 
 
 
 years = os.listdir() # listing years folders
 
+print(get_video_formats(data))
+
 for year in years:
+    
+    
+  
     os.chdir(year)
     files = os.listdir()
-    
+
     for file in files:
        
-       
-        """
         if file.endswith(".zip"):
            with zipfile.ZipFile(file,'r') as zip_ref:
                 zip_ref.extractall()
-        """  
-   
+        
+    os.chdir("..")
+
+    
+    for root, dirs, files in os.walk(year):
+        for file in files:
+            #try:
+            current_directory = os.getcwd()
+                
+            if file.split(".")[1] in formats:
+                video_path =os.path.join(root, file)
+                #video_folder=video_path.split("/")[len(video_path)-2]
+               
+               
+                video_data = list(filter(lambda x: x[4]==file,data)) # filtering the video by name in the .csv file
+                print(file,video_data)
+                   
+                if(video_data!=[]):
+                    extract_frames(video_data[0],video_path,root)
+                else:
+                    print("Video not found in the spreadshet")
+
+
+
+            """
+            except Exception as e :
+                
+                print(file,"THE FILE DOESN'T HAVE AN EXTENSION")
+                continue
+            """
+    """
+        if file.endswith(".zip"):
+           with zipfile.ZipFile(file,'r') as zip_ref:
+                zip_ref.extractall()
+    """  
+        
+    """
         if os.path.isdir(file):
             shutil.rmtree(file)
             continue
@@ -119,6 +184,7 @@ for year in years:
         
 
         print(os.listdir())
+    """
   
 
 
